7cef9967e4219e79ebaab324187c909118069860
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-06-19 20:52:56 +0200

    update

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,3 @@ import "bootstrap/dist/css/bootstrap.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Root />);
 
-//usunięcie loga mikrusa
-useEffect(() => {
-  const element = document.querySelector('p[style*="Powered by MIKR.US"]');
-  if (element) {
-    element.style.display = "none";
-  }
-}, []);
