403119b73d3216d4e68be30953df3b6d9bc4240a
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-06-19 20:22:49 +0200

    deleted logo of mikrus

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,7 @@ import "bootstrap/dist/css/bootstrap.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Root />);
 
+//usunięcie loga mikrusa
 useEffect(() => {
   const element = document.querySelector('p[style*="Powered by MIKR.US"]');
   if (element) {
