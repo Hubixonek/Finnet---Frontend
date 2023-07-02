742bbf0aae7eeb0d7f1e0ec93fd8f2dca4149140
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-07-02 17:15:38 +0200

    next update for navbar and working with rwd

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,27 @@
+import { FaBars } from "react-icons/fa";
+import styles from "../styles/Navbar.module.scss";
+import { Link } from "react-router-dom";
+const Nav = ({ setShowNav, showNav }) => {
+  return (
+    <nav className={styles["navbar"]}>
+      <label>
+        <FaBars
+          onClick={() => setShowNav(!showNav)}
+          className={styles["hamburger"]}
+        />
+      </label>
+      <div className={styles["logo"]}>
+        <span>Finnet</span>
+      </div>
+      <div className={styles["buttonGroup"]}>
+        <div className={styles["signInButton"]}>
+          <Link to="/loginform">Zaloguj</Link>
+        </div>
+        <div className={styles["registerNav"]}>
+          <Link to="/registerform">Korzystaj z Finnet za darmo</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Nav;
