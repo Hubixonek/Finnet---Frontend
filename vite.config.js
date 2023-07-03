76c834dc67f2cf7f0ccaac69aa5e3eeeeed2627d
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-07-03 20:52:41 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-icons"],
+export default defineConfig(() => {
+  return {
+    build: {
+      outDir: "build",
     },
-  },
+    plugins: [react()],
+  };
 });
