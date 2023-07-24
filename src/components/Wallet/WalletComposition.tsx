aa29120c0d1a37c8983ccc404aca6efa5ffaf728
Hubert Adamkowski <hubertadamkowskip56@gmail.com>
2023-07-24 12:25:48 +0200

    fix

--- src/components/Wallet/WalletComposition.tsx
+++ src/components/Wallet/WalletComposition.tsx
@@ -15,18 +15,18 @@ const WalletComposition = () => {
     LocalStorage.set("wallet", nameAndCurrencyWallet);
   }, [nameAndCurrencyWallet]);
 
-  if (nameAndCurrencyWallet == null) {
-    return null;
-  }
+  // if (nameAndCurrencyWallet == null) {
+  //   return null;
+  // }
   return (
     <div
       className={`${styles["container"]} ${
         theme ? styles["dark"] : styles["light"]
       }`}>
       <Form.Select className={styles["wallets"]}>
-        {nameAndCurrencyWallet.map((wallet) => (
+        {/* {nameAndCurrencyWallet.map((wallet) => (
           <option key={wallet.name}>{wallet.name}</option>
-        ))}
+        ))} */}
       </Form.Select>
 
       <h1>Portfel - skład i struktura</h1>
